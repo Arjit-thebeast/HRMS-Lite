098f9ff782eca9d2be48950af33eab140546f650
Arjit-thebeast <arjitaggarwal72004@gmail.com>
2026-02-09 18:47:59 +0530

    Great

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import List
 
-import models, schemas, crud
+import models, schemas, test
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -37,34 +37,34 @@ def get_db():
 
 @app.post("/employees/", response_model=schemas.Employee)
 def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_db)):
-    db_employee = crud.get_employee_by_email(db, email=employee.email)
+    db_employee = test.get_employee_by_email(db, email=employee.email)
     if db_employee:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_employee(db=db, employee=employee)
+    return test.create_employee(db=db, employee=employee)
 
 @app.get("/employees/", response_model=List[schemas.Employee])
 def read_employees(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return crud.get_employees(db, skip=skip, limit=limit)
+    return test.get_employees(db, skip=skip, limit=limit)
 
 @app.delete("/employees/{employee_id}", response_model=schemas.Employee)
 def delete_employee(employee_id: int, db: Session = Depends(get_db)):
-    db_employee = crud.delete_employee(db, employee_id)
+    db_employee = test.delete_employee(db, employee_id)
     if not db_employee:
         raise HTTPException(status_code=404, detail="Employee not found")
     return db_employee
 
 @app.post("/attendance/", response_model=schemas.Attendance)
 def create_attendance(attendance: schemas.AttendanceCreate, db: Session = Depends(get_db)):
-    return crud.create_attendance(db=db, attendance=attendance)
+    return test.create_attendance(db=db, attendance=attendance)
 
 @app.get("/attendance/{employee_id}", response_model=List[schemas.Attendance])
 def read_attendance(employee_id: int, db: Session = Depends(get_db)):
-    return crud.get_attendance(db, employee_id=employee_id)
+    return test.get_attendance(db, employee_id=employee_id)
 
 @app.get("/attendance/today/", response_model=List[schemas.Employee])
 def read_present_today(db: Session = Depends(get_db)):
-    return crud.get_present_today(db)
+    return test.get_present_today(db)
 
 @app.get("/stats/")
 def read_stats(db: Session = Depends(get_db)):
-    return crud.get_stats(db)
+    return test.get_stats(db)
